e9173b595ce7c369e7cd2d8016d6e94417c08f8d
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-02-10 22:22:16 +0200

    changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import platform
-
 import aiohttp
 import asyncio
 import pathlib
@@ -7,101 +6,122 @@ import sys
 import json
 from datetime import date, timedelta
 import re
+from aiopath import AsyncPath
+from aiofile import async_open
+import logging
 
-BASE_DIR = pathlib.Path()
+#BASE_DIR = pathlib.Path()
+#BASE_DIR.joinpath("storage/data.json")
 
-async def main(quantity_days):
-    
-    async with aiohttp.ClientSession() as session:       
-             
-        #param = {"date": "08.02.2023"}
-        
-        today = date.today()
-        quantity_days = 3
-        while quantity_days != 0:
-            days = quantity_days
-            pre_day = (today - timedelta(days)).strftime("%d.%m.%Y")
-            quantity_days -= 1 
-            #print(pre_day)
-            param = {"date": pre_day}
-            
-    
+async def request(param_list):
+    async with aiohttp.ClientSession() as session:    
+        for param in param_list:  
             try:           
                 async with session.get("https://api.privatbank.ua/p24api/exchange_rates?", params = param) as response:
                     if response.status == 200:
                         result = await response.json()
-                        #print(result)
+                        output_data(result)
                         
-                        def find_currency(requested_currency: str) -> dict: #знайти валюту в даних від приватбанку
-                            i = 0
-                            length_exchangeRate = len(result.get("exchangeRate"))
-                            while result.get("exchangeRate")[i].get("currency") != requested_currency:
-                                i += 1
-                                if i == length_exchangeRate:
-                                    print("Requested currency is not correct")
-                                    return None
-                                    
-                            requested_currency_Rate = result.get("exchangeRate")[i]
-                            
-                            #print(requested_currency_Rate)
-                            return requested_currency_Rate
-                        
-                        def read_json_file(): #читання даних з файла json
-                            with open(BASE_DIR.joinpath("storage/data.json"), 'r', encoding='utf-8') as fd:
-                                text = fd.readline()
-                                if text:
-                                    upload = json.loads(text)  
-                            return upload
-                        
-                        def write_json_file(output): #запис даних до файла json 
-                            upload = read_json_file()         
-                            with open(BASE_DIR.joinpath("storage/data.json"), 'w', encoding='utf-8') as fd:                
-                                upload.append(output)
-                                json.dump(upload, fd, ensure_ascii=False) 
-                        
-                        def output_for_file():
-                            all_value_Rates = {}
-                            for i in ["EUR", "USD"]: 
-                                answer_currency_Rate = find_currency(i)
-                                value_Rate = {
-                                    answer_currency_Rate.get("currency"): {
-                                        "sale": answer_currency_Rate.get("saleRate"), 
-                                        "purchase": answer_currency_Rate.get("purchaseRate")
-                                    }
-                                }
-                                all_value_Rates.update(value_Rate)
+                        #all_results.append(result)
+                    else: 
+                        logging.error(f"Error status: {response.status}")
+            except aiohttp.ClientConnectorError as err: 
+                logging.error(f"Connection error: {err}")
+    return None
     
-                            output = {
-                                pre_day: all_value_Rates
-                                }
-                            print([output]) 
-                            write_json_file(output)
-                            
-                            return
-                                
-                        output_for_file()
-                                
-                    else:
-                        print(f"Error status: {response.status}")
-            except aiohttp.ClientConnectorError as err:            
-                    print(f"Connection error:", str(err))
-                    #print(response.ok)
-                    
-                    #return result
+def find_currency(requested_currency: str, answer: dict) -> dict: #знайти валюту в даних від приватбанку
+    i = 0
+    length_exchangeRate = len(answer.get("exchangeRate"))
+    while answer.get("exchangeRate")[i].get("currency") != requested_currency:
+        i += 1
+        if i == length_exchangeRate:
+            print("Requested currency is not correct")
+            return None
+            
+    requested_currency_Rate = answer.get("exchangeRate")[i]       
+    #print(requested_currency_Rate)
+    return requested_currency_Rate, answer.get("date")    
+    
+#формується параметр дати для запиту данних в API
+#param = {"date": "10.02.2023"}   - приклад необхідного формату (на поточну дату відповідь сервера - 500???)
+async def date_param(quantity_days): 
+    quantity_days = int(quantity_days)
+    today = date.today()
+    param_list = []
+    while quantity_days != 0:
+        days = quantity_days
+        pre_day = (today - timedelta(days)).strftime("%d.%m.%Y")
+        quantity_days -= 1 
+        #print(pre_day)
+        param = {"date": pre_day}   
+        param_list.append(param) 
+    await request(param_list)
+    #for answer in api_answers:
+    return None
 
+# async def write_json_file(filename: str, queue: asyncio.Queue): #запис даних до файла json        
+#     async with async_open(filename, 'w', encoding='utf-8') as afd:                
+#         upload.append(output)
+#         json.dump(upload, afd, ensure_ascii=False) 
+                
+# async def read_json_file(file: AsyncPath, queue: asyncio.Queue): #читання даних з файла json
+#     async with async_open(file, 'r', encoding='utf-8') as afd:
+#         text = afd.readline()
+#         if text:
+#             upload = json.loads(text)  
+#     return upload
 
+def output_data(answer):
+    all_value_Rates = {}
+    for i in ["EUR", "USD"]: 
+        answer_currency_Rate, date = find_currency(i, answer)
+        value_Rate = {
+            answer_currency_Rate.get("currency"): {
+                "sale": answer_currency_Rate.get("saleRate"), 
+                "purchase": answer_currency_Rate.get("purchaseRate")
+            }
+        }
+        all_value_Rates.update(value_Rate)
+
+    output = {
+        date: all_value_Rates
+        }
+    print([output]) 
+    return #write_json_file(output)               
+
+async def main(num):
+    if 0 <= int(num) <= 10: 
+        await date_param(num)
+    else:    
+        print("Quantity days must be between 1 and 10")
+    return None
+    
+
+# async def run():
+#     files = AsyncPath('.').joinpath('files').glob('*.js')
+#     files_queue = asyncio.Queue()
+    
+#     file_reader = [asyncio.create_task(read_json_file(file, files_queue)) async for file in files]
+#     file_writer = asyncio.create_task(write_json_file('data.js', files_queue))
+    
+#     await asyncio.gather(*file_reader)
+#     await files_queue.join()
+#     file_writer.cancel()
+#     print('Completed')
+    
 if __name__ == "__main__":
-    STORAGE_DIR = pathlib.Path().joinpath('storage')
-    FILE_STORAGE = STORAGE_DIR / 'data.json'
+    # STORAGE_DIR = pathlib.Path().joinpath('storage')
+    # FILE_STORAGE = STORAGE_DIR / 'data.json'
     
-    if not FILE_STORAGE.exists():
-        with open(FILE_STORAGE, 'w', encoding='utf-8') as fd:
-            json.dump([], fd, ensure_ascii=False)
+    # if not FILE_STORAGE.exists():
+    #     with open(FILE_STORAGE, 'w', encoding='utf-8') as fd:
+    #         json.dump([], fd, ensure_ascii=False)
     try:
-       # num = sys.argv[1]
-       num = 2
+       num = sys.argv[1]
+       #num = 3
     except ValueError:
-        print("Entered count days to get request")        
+        print("Entered count days to get request")     
+           
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         
